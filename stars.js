32b8ee591e8feecf2bd16869349163fa14fcc561
itsw <sowonkim177@gmail.com>
2020-06-30 15:55:25 +0900

    update200630

--- stars.js
+++ stars.js
@@ -38,7 +38,7 @@ function userTable(username, menu, price, rank){
 function makeReviewTable(shopname, mapaddr, imgaddr, menu1, price1, rank1, menu2, price2, rank2, date){
     let divtag = "<div class='restaurant'>";
     divtag += "<h2> <a href='" + mapaddr +"'>" + shopname + "</a></h2>";
-    divtag += "<img src = '" + imgaddr + "'>"
+    divtag += imgaddr
     divtag += userTable("Ran", menu1, price1, rank1)
     divtag += userTable("Won", menu2, price2, rank2)
     divtag += "<div class='date'>" + date + "</div>"
@@ -47,30 +47,32 @@ function makeReviewTable(shopname, mapaddr, imgaddr, menu1, price1, rank1, menu2
 }
 
 
-// //이미지슬라이드
-// //참고링크 : https://nine8007.tistory.com/entry/%EC%B4%88%EA%B0%84%EB%8B%A8-%EC%9D%B4%EB%AF%B8%EC%A7%80-%EC%8A%AC%EB%9D%BC%EC%9D%B4%EB%93%9C-%EB%A7%8C%EB%93%A4%EA%B8%B0-Javascript
-// let firstImg = document.getElementsByClassName('img')[0]
-// let imgArr = [] //이미지여러개넣기
-// let imgIndex = 0;
+//이미지슬라이드
+//참고링크 : https://nine8007.tistory.com/entry/%EC%B4%88%EA%B0%84%EB%8B%A8-%EC%9D%B4%EB%AF%B8%EC%A7%80-%EC%8A%AC%EB%9D%BC%EC%9D%B4%EB%93%9C-%EB%A7%8C%EB%93%A4%EA%B8%B0-Javascript
+let firstImg = document.getElementById("slide")
+let imgArr = ["20200623buffet1.jpg", "20200623buffet2.jpg", "20200623buffet3.jpg"] //이미지여러개넣기
+let imgIndex = 0;
 
-// function imgslide(){
-//     firstImg.setAttribute("src",firstImg[imgIndex]);
-//     imgIndex++;
-//     if(imgIndex >= imgArr.length){
-//         imgIndex =0;
-//     }
-// }
+function imgslide(){
+    firstImg.setAttribute("src",firstImg[imgIndex]);
+    imgIndex++;
+    if(imgIndex >= imgArr.length){
+        imgIndex =0;
+    }
+}
+setInterval(imgslide, 3000)
 
-// setInterval(imgslide, 3000);
+
 
 
 
 //실제데이터 : shopname, mapaddr, imgaddr,  menu1, price1, rank1, menu2, price2, rank2, date
-document.write(makeReviewTable("각방", "http://naver.me/5LfKibcx", "http://blogfiles.naver.net/MjAxOTEyMDNfMTM4/MDAxNTc1MzM2NzQwNzEx.HB8dI7Z7-hlkGiURrRdzb4K7dlOZCCvxMNoxD-biydog.BmOh3STZ40M-vr7dcgW10MyxMTy6LXBGnphJCl72764g.JPEG.lovefts777/IMG_9915.JPG", "연어덮밥",  9000, 4, "소고기덮밥",  8000, 3, "2020-06-09"))
-document.write(makeReviewTable("갈비3선(구 서면대포집)", "http://naver.me/xrh6JnL3", "img/20200612.jpg", "돼지갈비찜정식",  10900, 4, "돼지갈비찜정식",  10900, 4, "2020-06-12"))
-document.write(makeReviewTable("맥도날드 서면3호점", "http://naver.me/xFQLgBF5", "img/20200616.jpg", "트리플치즈버거",  7900, 5, "베이컨토마토디럭스",  7800, 4, "2020-06-16"))
-document.write(makeReviewTable("에그드랍 전포카페거리", "http://naver.me/G3tKAIWF", "img/20200619eggdrop.jpg", "베이컨더블치즈",  7900, 4, "갈릭베이컨치즈",  8400, 5, "2020-06-19"))
-document.write(makeReviewTable("오쉐르웨딩홀 뷔페", "http://naver.me/xKJZDTrV", "img/20200623buffet1.jpg", "까르보나라,<br>목살스테이크",  9900, 5, "까르보나라, <br>목살스테이크",  9900, 5, "2020-06-23"))
+document.write(makeReviewTable("각방", "http://naver.me/5LfKibcx", "<img src = 'http://blogfiles.naver.net/MjAxOTEyMDNfMTM4/MDAxNTc1MzM2NzQwNzEx.HB8dI7Z7-hlkGiURrRdzb4K7dlOZCCvxMNoxD-biydog.BmOh3STZ40M-vr7dcgW10MyxMTy6LXBGnphJCl72764g.JPEG.lovefts777/IMG_9915.JPG'>", "연어덮밥",  9000, 4, "소고기덮밥",  8000, 3, "2020-06-09"))
+document.write(makeReviewTable("갈비3선(구 서면대포집)", "http://naver.me/xrh6JnL3", "<img src = 'img/20200612.jpg'>", "돼지갈비찜정식",  10900, 4, "돼지갈비찜정식",  10900, 4, "2020-06-12"))
+document.write(makeReviewTable("맥도날드 서면3호점", "http://naver.me/xFQLgBF5", "<img src = 'img/20200616.jpg'>", "트리플치즈버거",  7900, 5, "베이컨토마토디럭스",  7800, 4, "2020-06-16"))
+document.write(makeReviewTable("에그드랍 전포카페거리", "http://naver.me/G3tKAIWF", "<img src = 'img/20200619eggdrop.jpg'>", "베이컨더블치즈",  7900, 4, "갈릭베이컨치즈",  8400, 5, "2020-06-19"))
+document.write(makeReviewTable("오쉐르웨딩홀 뷔페", "http://naver.me/xKJZDTrV", "<img src = 'img/20200623buffet1.jpg' id='slide'>" , "까르보나라,<br>목살스테이크",  9900, 5, "까르보나라, <br>목살스테이크",  9900, 5, "2020-06-23"))
+document.write(makeReviewTable("원카츠", "http://naver.me/GfZ5HYEE", "<img src = 'img/20200630.jpg'>" , "철판치즈카츠",  9500, 5, "등심카츠",  6000, 5, "2020-06-30"))
 
 
 //예시
