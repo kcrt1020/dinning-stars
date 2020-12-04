32b51d107ff6aed6c8544cc2b2ac1d07c46dc9d2
sowon <sowonkim177@gmail.com>
2020-12-04 21:04:05 +0900

    [feat]imgSlide

--- stars.js
+++ stars.js
@@ -1,4 +1,4 @@
-//숫자데이터 입력받으면 검정별을 숫자데이터만큼 찍기 + 공백은 텅빈별찍기
+//숫자데이터 입력받으면 검정별을 숫자데이터만큼 찍기 + 공백은 빈 별찍기
 function rankToStar(rank){
     switch(rank){
         case 5 : 
@@ -46,28 +46,11 @@ function makeReviewTable(shopname, mapaddr, imgaddr, menu1, price1, rank1, menu2
     return divtag;
 }
 
-//이미지슬라이드
-//참고링크 : https://nine8007.tistory.com/entry/%EC%B4%88%EA%B0%84%EB%8B%A8-%EC%9D%B4%EB%AF%B8%EC%A7%80-%EC%8A%AC%EB%9D%BC%EC%9D%B4%EB%93%9C-%EB%A7%8C%EB%93%A4%EA%B8%B0-Javascript
-let firstImg = document.getElementById("slide")
-let imgArr = ["20200623buffet1.jpg", "20200623buffet2.jpg", "20200623buffet3.jpg"] //이미지여러개넣기
-let imgIndex = 0;
-
-function imgslide(){
-    firstImg.setAttribute("src",firstImg[imgIndex]);
-    imgIndex++;
-    if(imgIndex >= imgArr.length){
-        imgIndex =0;
-    }
-}
-//setInterval(imgslide, 3000)
-
-
-
 //데이터입력예시
 //document.write(makeReviewTable("예시", "#", "20200612.jpg", "음식메뉴",  10000, 1, "음식메뉴",  100000, 2, "2020-06-12"))
 
 //실제데이터 : shopname, mapaddr, imgaddr,  menu1, price1, rank1, menu2, price2, rank2, date
-document.write(makeReviewTable("각방", "http://naver.me/5LfKibcx", "<img src = 'https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=https%3A%2F%2Fmyplace-phinf.pstatic.net%2F20200517_220%2F15897096292959oYfK_JPEG%2Fupload_b110d1cc89ffa7556b748d53a46d9bed.jpeg' alt='네이버플레이스로 이동합니다.'>", "연어덮밥",  9000, 4, "소고기덮밥",  8000, 3, "2020-06-09"))
+document.write(makeReviewTable("각방", "http://naver.me/5LfKibcx", "<img src = 'img/20200609.jpg'>", "연어덮밥",  9000, 4, "소고기덮밥",  8000, 3, "2020-06-09"))
 document.write(makeReviewTable("갈비3선(구 서면대포집)", "http://naver.me/xrh6JnL3", "<img src = 'img/20200612.jpg'>", "돼지갈비찜정식",  10900, 4, "돼지갈비찜정식",  10900, 4, "2020-06-12"))
 document.write(makeReviewTable("맥도날드 서면3호점", "http://naver.me/xFQLgBF5", "<img src = 'img/20200616.jpg'>", "트리플치즈버거",  7900, 5, "베이컨토마토디럭스",  7800, 4, "2020-06-16"))
 document.write(makeReviewTable("에그드랍 전포카페거리", "http://naver.me/G3tKAIWF", "<img src = 'img/20200619eggdrop.jpg'>", "베이컨더블치즈",  7900, 4, "갈릭베이컨치즈",  8400, 5, "2020-06-19"))
@@ -81,3 +64,19 @@ document.write(makeReviewTable("건양정홍삼삼계탕", "http://naver.me/xBDI
 document.write(makeReviewTable("마스타철판/이오카츠", "http://naver.me/xxaBpNij", "<img src = 'img/20200731.jpg'>" , "해물철판볶음우동", 7900, 4, "이오카츠정식",  9900, 3, "2020-07-31"))
 document.write(makeReviewTable("가득족발", "http://naver.me/FoASWj4F", "<img src = 'img/20200804.jpg'>" , "족발정식", 8000, 4, "족발정식",  8000, 5, "2020-08-04"))
 
+//이미지슬라이드
+//참고링크 : https://nine8007.tistory.com/entry/%EC%B4%88%EA%B0%84%EB%8B%A8-%EC%9D%B4%EB%AF%B8%EC%A7%80-%EC%8A%AC%EB%9D%BC%EC%9D%B4%EB%93%9C-%EB%A7%8C%EB%93%A4%EA%B8%B0-Javascript
+let firstImg = document.getElementById("slide");
+console.log(firstImg);
+let imgArr = ["img/20200623buffet1.jpg", "img/20200623buffet2.jpg", "img/20200623buffet3.jpg"] //이미지여러개넣기
+let imgIndex = 0;
+
+function imgslide(){
+    firstImg.setAttribute("src", imgArr[imgIndex]);
+    imgIndex++;
+
+    if(imgIndex >= imgArr.length){
+        imgIndex =0;
+    }
+}
+setInterval(imgslide, 2000);
